0302d9f1159b2188d055b0d23ad59f0a30cab888
Joe Cowton <joecowton@gmail.com>
2018-02-02 11:46:59 +0000

    tweaks

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -45,7 +45,7 @@ class SearchBar extends Component {
 }
 
 function  mapDispatchToProps(dispatch) {
-  return bindActionCreators( {fetchWeather}, dispatch);
+  return bindActionCreators({fetchWeather}, dispatch);
 }
 
 export default connect(null, mapDispatchToProps)(SearchBar);
