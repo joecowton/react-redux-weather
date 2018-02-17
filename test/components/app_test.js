0bfda1ae6f191fd300cce9e9755f99b955ecf7d4
Joe Cowton <joecowton@gmail.com>
2018-02-17 13:04:06 +0000

    heroku webpack issue

--- test/components/app_test.js
+++ test/components/app_test.js
@@ -8,7 +8,7 @@ describe('App' , () => {
     component = renderComponent(App);
   });
 
-  it('renders something', () => {
+  it('renders something..', () => {
     expect(component).to.exist;
   });
 
